5912a02853d16d1be315fbf80d96d7e41edde2a6
ashutosh thakur <105896076+ashutoshthakur47@users.noreply.github.com>
2023-06-30 21:59:42 +0530

    Add files via upload

---
+++ line graph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x=[1,4,7,9]
+y=[2,3,6,5]
+plt.plot(x,y,marker='o',linestyle='-.')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title("A simple line graph")
+plt.show()
