2517d9399eda533664a27a18b2481560e2220bf2
ashutosh thakur <105896076+ashutoshthakur47@users.noreply.github.com>
2023-06-30 22:07:03 +0530

    Update line graph.py

--- line graph.py
+++ line graph.py
@@ -1,9 +1,19 @@
+#This line imports the pyplot module from the matplotlib library and assigns it the alias plt
 import matplotlib.pyplot as plt
 
+#Here you write the coordinates 
 x=[1,4,7,9]
 y=[2,3,6,5]
-plt.plot(x,y,marker='o',linestyle='-.')
+
+# This line plots the data points using the plot() function
+plt.plot(x,y,)
+
+#This will give the names to the x-axis and y-axis
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
+
+#This line sets the title of the plot as "A simple line graph".
 plt.title("A simple line graph")
+
+#This line displays the plot on the screen.
 plt.show()
